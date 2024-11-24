33b804612e2dffc0f1ada0371f87b392a7db1d17
devopclinics <info@devopclinics.com>
2024-11-24 10:03:15 -0600

    ubu14

--- run_gotty.sh
+++ run_gotty.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-service ssh start
-/usr/local/bin/gotty -w --port 80 --address 0.0.0.0 /bin/bash
+# service ssh start
+# /usr/local/bin/gotty -w --port 80 --address 0.0.0.0 /bin/bash
+
+
+
+/usr/local/bin/gotty --permit-write --reconnect /bin/bash
