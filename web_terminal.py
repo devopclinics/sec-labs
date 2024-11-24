538100355726cc4a0aa47e63a37ee614cb1b1fb3
devopclinics <info@devopclinics.com>
2024-11-24 12:37:22 -0600

    v4

--- web_terminal.py
+++ web_terminal.py
@@ -4,7 +4,7 @@ import subprocess
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 @app.route('/')
 def index():
